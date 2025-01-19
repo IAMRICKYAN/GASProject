8ad96d6dcaeff9214c70d66ccfb2c01c4fed56d6
YANZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2025-01-19 20:51:09 +0800

    创建Aura Player Controller

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "EnhancedInputSubsystems.h"
 #include "AuraPlayerController.generated.h"
+
+
+class UInputMappingContext;
 
 /**
  * 
@@ -13,5 +17,14 @@ UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere,Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
 };
