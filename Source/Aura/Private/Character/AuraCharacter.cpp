7abf0198440574b97802cb15f7c871850318dd51
YANZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2025-01-24 03:22:36 +0800

    GameMode设置
    - 设置GameMode
    - 简单修改动画蓝图
    - 调整角色视角

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,16 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true; // 确保角色的旋转方向与移动方向相同
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f); 
+	GetCharacterMovement()->bConstrainToPlane = true; // 限制角色移动到平面上
+	GetCharacterMovement()->bSnapToPlaneAtStart = true; // 角色开始时 snap to plane
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright RickYan
+
+
+#include "Game/AuraGameModeBase.h"
+
