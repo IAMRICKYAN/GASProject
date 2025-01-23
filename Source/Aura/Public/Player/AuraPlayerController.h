a8845ab34d3a2917016add17f52fba656a4dd1db
YANZHANYUAN <108991281+IAMRICKYAN@users.noreply.github.com>
2025-01-24 03:47:24 +0800

    高亮敌人

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -13,6 +13,7 @@ class UInputMappingContext;
 class UInputAction;
 
 struct FInputActionValue;
+class IEnemyInterface;
 
 /**
  * 
@@ -24,6 +25,7 @@ class AURA_API AAuraPlayerController : public APlayerController
 
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
 
 protected:
 	virtual void BeginPlay() override;
@@ -37,4 +39,8 @@ private:
 	TObjectPtr<UInputAction> MoveAction;
 
 	void Move(const FInputActionValue& InputActionValue);
+
+	void CursorTrace();
+	IEnemyInterface* LastActor;
+	IEnemyInterface* ThisActor;
 };
